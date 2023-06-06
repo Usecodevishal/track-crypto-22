275ad51b02e045440656f8f664ea3e5f5a9e0c95
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-06-06 09:55:32 +0530

    Update index.js

--- src/components/Common/Button/index.js
+++ src/components/Common/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 function Button({text, outlined}) {
     return (
-        <p className={outlined?"outlined-btn":"btn"}>{text}</p>
+        <div className={outlined?"outlined-btn-div":"btn-div"}>{text}</div>
     )
 }
 
